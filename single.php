58da601df74f823c578f441a8022e07a592b6455
Patryck Gratão <patryckgratao@gmail.com>
2015-01-01 07:55:22 -0200

    Colocando links com sublinhado

--- single.php
+++ single.php
@@ -26,10 +26,12 @@
             <figure class="imagem_destacada">
                 <?php the_post_thumbnail() ?>
             </figure>
-             <figcaption>Isso é uma legenda</figcaption>
 
             <div id="conteudo_post">
                  <?php the_content(); ?>
+
+
+
             </div>
 
         <?php endwhile; ?>
